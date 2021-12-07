4f38f3c0ddc53a8d6c01b44214b84b1bb8c47a62
Aakash Patil <patil.aa@northeastern.edu>
2021-12-07 12:48:31 -0500

    Data object role for counselor role implementation

--- AdoptionAssist/src/main/java/Business/Role/CounselorRole.java
+++ AdoptionAssist/src/main/java/Business/Role/CounselorRole.java
@@ -5,10 +5,23 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author nikhi
  */
-public class CounselorRole {
+public class CounselorRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new CounselorWorkAreaJPanel(userProcessContainer, account, organization, enterprise);
+    }
+    
     
 }
+
