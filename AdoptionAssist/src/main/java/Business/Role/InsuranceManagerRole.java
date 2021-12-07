0713422b13f72f2307a109a7e1d5ccc6a2fd5fc1
Aakash Patil <patil.aa@northeastern.edu>
2021-12-07 15:49:59 -0500

    Role implementation commit

--- AdoptionAssist/src/main/java/Business/Role/InsuranceManagerRole.java
+++ AdoptionAssist/src/main/java/Business/Role/InsuranceManagerRole.java
@@ -5,10 +5,23 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author nikhi
  */
-public class InsuranceManagerRole {
+public class InsuranceManagerRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new ViewInsuranceApplicationWorkAreaJPanel(userProcessContainer, enterprise, account);
+    }
+
+    
     
 }
