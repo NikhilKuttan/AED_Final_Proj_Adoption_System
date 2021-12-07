c2ed0e4227695acf25d656766054ab70c14d363c
Aakash Patil <patil.aa@northeastern.edu>
2021-12-07 12:43:15 -0500

    Parents login role data objects

--- AdoptionAssist/src/main/java/Business/Role/ParentsRole.java
+++ AdoptionAssist/src/main/java/Business/Role/ParentsRole.java
@@ -5,10 +5,20 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author nikhi
  */
-public class ParentsRole {
+public class ParentsRole extends Role{
+     @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new ParentsWorkAreaJPanel(userProcessContainer, enterprise, account, organization);
+    }
     
 }
