cde24633cc4767fd8c20353e7df2072b4d1ca5b7
Aakash Patil <patil.aa@northeastern.edu>
2021-12-07 12:45:48 -0500

    sysadmin role data object class

--- AdoptionAssist/src/main/java/Business/Role/SystemAdminRole.java
+++ AdoptionAssist/src/main/java/Business/Role/SystemAdminRole.java
@@ -5,10 +5,22 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
- * @author nikhi
+ * @author patil
  */
-public class SystemAdminRole {
+public class SystemAdminRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem system) {
+        return new SystemAdminWorkAreaJPanel(userProcessContainer, system);
+    }
     
 }
+
