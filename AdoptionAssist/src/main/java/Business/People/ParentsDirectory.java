67ed1a1ce67370c79064ed14deb66cab11356e4e
Aakash Patil <patil.aa@northeastern.edu>
2021-12-07 12:30:37 -0500

    Parents data object classes and directory

--- AdoptionAssist/src/main/java/Business/People/ParentsDirectory.java
+++ AdoptionAssist/src/main/java/Business/People/ParentsDirectory.java
@@ -5,10 +5,34 @@
  */
 package Business.People;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author nikhi
  */
 public class ParentsDirectory {
     
+    private ArrayList<Parents> parentsList;
+
+    public ParentsDirectory() {
+        parentsList = new ArrayList();
+    }
+
+    public ArrayList<Parents> getParentsList() {
+        return parentsList;
+    }
+    
+    
+    public Parents createParents(String name){
+        Parents parents = new Parents();
+        parents.setUsername(name);
+        parentsList.add(parents);
+        return parents;
+    }
+    
+    public Parents addParents(Parents parent){
+        parentsList.add(parent);
+        return parent;
+    }
 }
