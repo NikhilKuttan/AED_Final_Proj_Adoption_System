22afbc3881b6b9e306053b9135917583d0178695
Aakash Patil <patil.aa@northeastern.edu>
2021-12-07 12:46:24 -0500

    Bank manager roles data object class

--- AdoptionAssist/src/main/java/Business/Role/BankManagerRole.java
+++ AdoptionAssist/src/main/java/Business/Role/BankManagerRole.java
@@ -5,10 +5,21 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
- * @author nikhi
+ * @author patil
  */
-public class BankManagerRole {
+public class BankManagerRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new BankManagerWorkAreaJPanel(userProcessContainer, account, organization, business, enterprise);
+    }
     
 }
