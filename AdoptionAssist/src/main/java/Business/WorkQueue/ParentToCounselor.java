cd04dbddb801d50a99f198f03ab6015e56d2530a
simranpatil30 <91101877+simranpatil30@users.noreply.github.com>
2021-12-07 22:44:32 -0500

    Committing ParentToCounselor file

--- AdoptionAssist/src/main/java/Business/WorkQueue/ParentToCounselor.java
+++ AdoptionAssist/src/main/java/Business/WorkQueue/ParentToCounselor.java
@@ -4,11 +4,49 @@
  * and open the template in the editor.
  */
 package Business.WorkQueue;
-
+import Business.People.Parents;
+import Business.UserAccount.UserAccount;
+import java.util.Date;
 /**
  *
- * @author nikhi
+ * @author simran
  */
-public class ParentToCounselor {
-    
+public class ParentToCounselor extends WorkRequest{
+    private String requestResult;
+    private Parents parent;
+    private String counsellorFeeback;
+
+    public ParentToCounselor(String message, Parents parent) {
+        super();
+        super.setMessage(message);
+        super.setStatus("Pending");
+        this.requestResult = new String();
+        this.parent = parent;
+        this.counsellorFeeback = new String();   
+    }
+
+    public String getRequestResult() {
+        return requestResult;
+    }
+
+    public void setRequestResult(String requestResult) {
+        this.requestResult = requestResult;
+    }
+
+    public Parents getParent() {
+        return parent;
+    }
+
+    public void setParent(Parents p) {
+        this.parent = p;
+    }
+
+    public String getCounsellorFeeback() {
+        return counsellorFeeback;
+    }
+
+    public void setCounsellorFeeback(String counsellorFeeback) {
+        this.counsellorFeeback = counsellorFeeback;
+    }
 }
+
