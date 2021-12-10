70c1981478605dfe81238d704f12636880010526
NikhilKuttan <kuttan.n@northeastern.edu>
2021-12-09 19:30:40 -0500

    Commiting files

--- AdoptionAssist/src/main/java/Business/Enterprise/HospitalEnterprise.java
+++ AdoptionAssist/src/main/java/Business/Enterprise/HospitalEnterprise.java
@@ -5,10 +5,21 @@
  */
 package Business.Enterprise;
 
+import Business.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author nikhi
  */
-public class HospitalEnterprise {
+public class HospitalEnterprise extends Enterprise {
+    
+    public HospitalEnterprise(String name){
+        super(name,EnterpriseType.Hospital);
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
     
 }
