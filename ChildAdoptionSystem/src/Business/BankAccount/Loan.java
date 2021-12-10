0c7f737db723eb21fff66618c2e6cf6426861043
simranpatil30 <91101877+simranpatil30@users.noreply.github.com>
2021-12-10 18:45:16 -0500

    Committing changes to business files

--- ChildAdoptionSystem/src/Business/BankAccount/Loan.java
+++ ChildAdoptionSystem/src/Business/BankAccount/Loan.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 /**
  *
- * @author Sebsebin
+ * @author simran
  */
 public class Loan {
     
--- ChildAdoptionSystem/src/Business/BankAccount/LoanAccountDirectory.java
+++ ChildAdoptionSystem/src/Business/BankAccount/LoanAccountDirectory.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author ishanibose
+ * @author simran
  */
 public class LoanAccountDirectory {
         private ArrayList<Loan> loanList;
